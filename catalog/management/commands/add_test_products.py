2a626e59170ad9955f660bffa8c8dad959c90240
Vitaly Shcheglov <cgfhnfr4@gmail.com>
2025-05-06 08:25:40 +0500

    modified files

--- catalog/management/commands/add_test_products.py
+++ catalog/management/commands/add_test_products.py
@@ -6,6 +6,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         Product.objects.all().delete()  # удаляем старые продукты
-        # добавляем новые
-        category = Category.objects.get(id=18)
-        Product.objects.create(name="Новый продукт", description="Описание нового продукта", image="path/to/image.jpg", category=category, price=200.00)
+        Category.objects.all().delete() # удаляем старые категории
+        category = Category.objects.create(name="Тестовая категория", description="Описание тестовой категории")  # Создаем новую категорию
+        Product.objects.create(name="Новый продукт", description="Описание нового продукта", image="path/to/image.jpg", category=category, price=200.00) # Добавляем новые продукты
